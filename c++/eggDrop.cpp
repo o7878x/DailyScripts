c9a30762781178bf1e5f88985705d9080824654b
shark <dongcai.mo@gmail.com>
2017-12-14 22:24:28 +0800

    Add dynammic programming one

--- c++/eggDrop.cpp
+++ c++/eggDrop.cpp
@@ -26,8 +26,40 @@ int eggDrop(int n, int k) {
     return min+1;
 }
 
+// Dynamic planning method
+int eggDropEx(int n, int k) {
+    /* A 2D table where entry eggFloor[i][j] will represent minimum number of n eggs and k floors */
+    int eggFloor[n+1][k+1];
+
+    // boundary condition
+    for (int i = 1; i <= n; i++) {
+        eggFloor[i][0] = 0;
+        eggFloor[i][1] = 1;
+    }
+
+    for (int j = 1; j <= k; j++) {
+        eggFloor[1][j] = j;
+    }
+
+    // Fill rest of the entries in table using optimal substructure property
+    for (int i = 2; i <= n; i++) {
+        for (int j = 2; j <= k; j++) {
+            eggFloor[i][j] = INT_MAX;
+            for (int x = 1; x <= j; x++) {
+                int res = 1 + max(eggFloor[i-1][x-1] , eggFloor[i][j-x]);
+                if (res < eggFloor[i][j]) {
+                    eggFloor[i][j] = res;
+                }
+            }
+        }
+    }
+
+    return eggFloor[n][k];
+}
+
+
 int main(void) {
-    int n = 2, k = 20;
-    printf("\nMinimum number of trials in worst case with %d eggs and %d floors is %d\n", n, k, eggDrop(n, k));
+    int n = 2, k = 100;
+    printf("\nMinimum number of trials in worst case with %d eggs and %d floors is %d\n", n, k, eggDropEx(n, k));
     return 0;
 }
