274b065979e9654e5062a7108aa78efcaed68b9b
shark <dongcai.mo@gmail.com>
2017-12-19 21:10:45 +0800

    Modify insetion sort algorithm

--- c++/insertionSort.cpp
+++ c++/insertionSort.cpp
@@ -1,15 +1,8 @@
 #include <cstdio>
 #include <cmath>
 
-
-/* Function to sort an array using insertion sort
- * Time Complexity: O(n*n)
- * Auxiliary Space: O(1)
- * Boundary Cases: Insertion sort takes maximum time to sort if elements are sortdd in reverse oder. And it takes minimum time (order of n) when elemnets are already sorted.
- */
 void insertionSort(int arr[], int n) {
     int key, i, j;
-
     for (i = 1; i < n; i++) {
         key = arr[i];
         for (j = i-1; j >= 0 && arr[j] > key; j--) {
@@ -19,21 +12,18 @@ void insertionSort(int arr[], int n) {
     }
 }
 
-// A utility function to print an array of size n
-void printArray(int arr[], int n) {
+void printResult(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-/* Driver program to test insertion sort */
 int main(void) {
-    int arr[] = { 12, 11, 13, 5, 6 };
+    int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
     int n = sizeof(arr)/sizeof(arr[0]);
-
     insertionSort(arr, n);
-    printArray(arr, n);
+    printResult(arr, n);
 
     return 0;
 }
