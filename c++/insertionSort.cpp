0c47f4bc9001abdbf38110051e009c4a0cffa318
shark <dongcai.mo@gmail.com>
2017-12-19 21:22:29 +0800

    Update insertion sort algorithm

--- c++/insertionSort.cpp
+++ c++/insertionSort.cpp
@@ -12,6 +12,17 @@ void insertionSort(int arr[], int n) {
     }
 }
 
+void insertionSortRe(int arr[], int n) {
+    int key, i, j;
+    for (i = n-2; i >= 0; i--) {
+        key = arr[i];
+        for (j = i+1; j < n && arr[j] < key; j++) {
+            arr[j-1] = arr[j];
+        }
+        arr[j-1] = key;
+    }
+}
+
 void printResult(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -22,7 +33,7 @@ void printResult(int arr[], int n) {
 int main(void) {
     int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
     int n = sizeof(arr)/sizeof(arr[0]);
-    insertionSort(arr, n);
+    insertionSortRe(arr, n);
     printResult(arr, n);
 
     return 0;
